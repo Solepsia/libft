752a99ceb01c2f8f84b115111f7974fb6d552de2
Gino ROTA <grota@e2r11p17.42.fr>
2017-11-20 13:35:35 +0100

    Headers OK, reste la norme, restent 6 fonctions + putbase

--- Makefile
+++ Makefile
@@ -6,13 +6,22 @@
 #    By: grota <marvin@42.fr>                       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/11/16 16:05:58 by grota             #+#    #+#              #
-#    Updated: 2017/11/16 18:56:21 by grota            ###   ########.fr        #
+#    Updated: 2017/11/20 13:30:07 by grota            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = libft.a
 
-SRC = ft_memset.c ft_bzero.c ft_memcpy.c ft_memccpy.c
+SRC = ft_memset.c ft_bzero.c ft_memcpy.c ft_memccpy.c ft_isalnum.c \
+	  ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isspace.c ft_memalloc.c \
+	  ft_memdel.c ft_putchar.c ft_putchar_fd.c ft_putendl.c \
+	  ft_putendl_fd.c ft_putnbr.c ft_putnbr_fd.c ft_putstr.c ft_putstr_fd.c \
+	  ft_strcat.c ft_strchr.c ft_strclr.c ft_strcmp.c ft_strcpy.c ft_strdel.c \
+	  ft_strdup.c ft_strequ.c ft_striter.c ft_striteri.c ft_strjoin.c \
+	  ft_strlcat.c ft_strlen.c ft_strmap.c ft_strmapi.c ft_strncat.c \
+	  ft_strncmp.c ft_strncpy.c ft_strnequ.c ft_strnew.c ft_strnstr.c \
+	  ft_strrchr.c ft_strstr.c ft_strsub.c ft_strtrim.c ft_tolower.c \
+	  ft_toupper.c
 
 OBJ = $(SRC:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: grota <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 14:48:49 by grota             #+#    #+#             */
-/*   Updated: 2017/11/16 18:56:18 by grota            ###   ########.fr       */
+/*   Updated: 2017/11/20 13:30:17 by grota            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,50 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memalloc(size_t size);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isspace(int c);
+void	ft_memdel(void **ap);
+void	ft_putchar(char c);
+void	ft_putstr(const char *s);
+void	ft_putendl(const char *s);
+void	ft_putnbr(int n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(const char *s, int fd);
+void	ft_putendl_fd(const char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strcpy(char *s1, const char *s2);
+char	*ft_strncpy(char *s1, const char *s2, size_t n);
+char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strncat(char *s1, const char *s2, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *s1, const char *s2);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnew(size_t size);
+void	ft_strdel(char **as);
+void	ft_strclr(char *s);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strequ(char const *s1, char const *s2);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s);
+char	**ft_strsplit(char const *s, char c);
+int		ft_atoi(const char *s);
+char	*ft_itoa(int n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
